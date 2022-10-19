0085b247b7d428766c0296ef8f2a5031c0ff2817
Basel Askar <baselaskar91@gmail.com>
2022-10-19 09:02:24 +0200

    make script in the same file

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-import TodoItem from "./todo_item.js";
+// import TodoItem from "./todo_item.js";
+
+//todo item class
+let currentId = 0;
+
+class TodoItem {
+  #id;
+  #content;
+  #done;
+
+  constructor(content){
+    this.#id = ++currentId;
+    this.#content = content;
+    this.#done = false;
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get content(){
+    return this.#content;
+  }
+
+  get done(){
+    return this.#done;
+  }
+
+  set done(value){
+    if (typeof(value) !== 'boolean'){
+      console.error('the value has to be boolean');
+      return;
+    }
+
+    this.#done = value;
+  }
+
+}
+
 
 
 
